94c292fefa3202b9d888559643ec7400dfb15b0e
roman <roman.qwonix@gmail.com>
2022-03-31 18:06:20 +0300

    lab 7-8: add json save and pdb debug

--- myform.py
+++ myform.py
@@ -1,5 +1,9 @@
+from os.path import exists
+
 from bottle import post, request
 from re import compile as regex_compile
+import pdb
+import json
 
 
 @post('/form', method='post')
@@ -13,5 +17,18 @@ def my_form():
         return f"Ошибка: email не соответствует шаблону"
     elif question == "":
         return f"Ошибка: введите ваш вопрос"
+
+    # pdb.set_trace()
+    questions = {}
+    if exists('json_data.json'):
+        with open('json_data.json', 'r', encoding='utf-8') as read_json:
+            questions = json.load(read_json)
+
+    with open('json_data.json', 'w', encoding='utf-8') as write_json:
+        if email in questions:
+            questions.get(email).append(question)
+        else:
+            questions[email] = [question]
+        json.dump(questions, write_json)
 
     return f"Спасибо! Ответ будет отправлен на почту: {email}!"
