0526000c7f686bcb06fa93c21b2f856c0cf7b3fd
Alex V <alexwolf2017@outlook.com>
2022-05-18 11:31:40 +0300

    fixed tests

--- myform.py
+++ myform.py
@@ -6,6 +6,28 @@ from routes import base_page, menu
 import json
 
 
+# функция для проверки почты
+def is_valid_email(email: str) -> bool:
+    email_pattern = regex_compile(r"([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+")
+    arr = email.split('@')
+    return bool(email_pattern.match(email)) and len(arr[0]) <= 64 and len(arr[1]) <= 255
+
+
+# функция для проверки заголовка
+def is_valid_title(title: str) -> bool:
+    return title != "" and 20 <= len(title) <= 100
+
+
+# функция для проверки описания
+def is_valid_description(description: str) -> bool:
+    return description != "" and 60 <= len(description) <= 1000
+
+
+# функция для проверки никнейма
+def is_valid_nickname(nickname: str) -> bool:
+    nickname_pattern = regex_compile(r"^\w{3,16}$")
+    return bool(nickname_pattern.match(nickname))
+
 def isCorrectMail(email: str):
     pattern = regex_compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
     if pattern.match(email):
